01f91260489455103d3a18f87dc0c55f533dc15a
songchenglong04 <songchenglong04@meituan.com>
2022-03-20 17:05:37 +0800

    feat: 修改配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ["plugin:react/recommended", "google"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      tsx: true,
+      jsx: true,
     },
     ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
   },
